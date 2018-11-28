5c574bf387162452470782d61ac649a6c0d22d60
MarkTheLoot <44461084+MarkTheLoot@users.noreply.github.com>
2018-11-28 23:54:28 +0100

    Aufräumarbeiten
    - Löschen von nicht benötigten Datein um ordnung zu schaffen.
    - Hinzufügen eines eigenen Test-Tilesets
    - Lizenzen hinzugefügt
    - player.gd abgespeckt, nicht genuzte funktionen entfernt
    - player.gd abgeändert das der sprung zum (noch endlosen) jetpack wird

--- player.gd
+++ player.gd
@@ -7,13 +7,10 @@ const MIN_ONAIR_TIME = 0.1
 const WALK_SPEED = 250 # pixels/sec
 const JUMP_SPEED = 480
 const SIDING_CHANGE_SPEED = 10
-const BULLET_VELOCITY = 1000
-const SHOOT_TIME_SHOW_WEAPON = 0.2
 
 var linear_vel = Vector2()
 var onair_time = 0 #
 var on_floor = false
-var shoot_time=99999 #time since last shot
 
 var anim=""
 
@@ -24,7 +21,6 @@ func _physics_process(delta):
 	#increment counters
 
 	onair_time += delta
-	shoot_time += delta
 
 	### MOVEMENT ###
 
@@ -50,20 +46,9 @@ func _physics_process(delta):
 	target_speed *= WALK_SPEED
 	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
 
-	# Jumping
-	if on_floor and Input.is_action_just_pressed("jump"):
+	# Jetpack
+	if Input.is_action_pressed("jump"):
 		linear_vel.y = -JUMP_SPEED
-		$sound_jump.play()
-
-	# Shooting
-	if Input.is_action_just_pressed("shoot"):
-		var bullet = preload("res://bullet.tscn").instance()
-		bullet.position = $sprite/bullet_shoot.global_position #use node for shoot position
-		bullet.linear_velocity = Vector2(sprite.scale.x * BULLET_VELOCITY, 0)
-		bullet.add_collision_exception_with(self) # don't want player to collide with bullet
-		get_parent().add_child(bullet) #don't want bullet to move with me, so add it as child of parent
-		$sound_shoot.play()
-		shoot_time = 0
 
 	### ANIMATION ###
 
@@ -78,9 +63,6 @@ func _physics_process(delta):
 			sprite.scale.x = 1
 			new_anim = "run"
 	else:
-		# We want the character to immediately change facing side when the player
-		# tries to change direction, during air control.
-		# This allows for example the player to shoot quickly left then right.
 		if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
 			sprite.scale.x = -1
 		if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
@@ -90,9 +72,6 @@ func _physics_process(delta):
 			new_anim = "jumping"
 		else:
 			new_anim = "falling"
-
-	if shoot_time < SHOOT_TIME_SHOW_WEAPON:
-		new_anim += "_weapon"
 
 	if new_anim != anim:
 		anim = new_anim
